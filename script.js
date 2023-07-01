e8252928f620fd376ba10a02a67ed0d53a016061
Erik Aakre <erikaakre@hotmail.com>
2023-07-01 11:46:27 -0500

    Hidden mobile menu on scroll

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ headerMenu.addEventListener("click", (event) => {
   event.preventDefault();
   nav.classList.toggle("active");
 });
+
+const header = document.querySelector(".header");
+let lastScrollY = window.scrollY;
+
+window.addEventListener("scroll", () => {
+  if (lastScrollY < window.scrollY) {
+    header.classList.add("header--hidden");
+    console.log("we are going down");
+  } else {
+    header.classList.remove("header--hidden");
+    console.log("we are going up");
+  }
+
+  lastScrollY = window.scrollY;
+});
