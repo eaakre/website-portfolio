6845404f6a9ef90cf31417ffbbf903ea3e9f62f2
Erik Aakre <erikaakre@hotmail.com>
2023-07-01 11:07:38 -0500

    prevent default on mobile menu

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const headerMenu = document.getElementsByClassName("header__menu")[0];
 const nav = document.getElementsByClassName("nav")[0];
 
-headerMenu.addEventListener("click", () => {
+headerMenu.addEventListener("click", (event) => {
+  event.preventDefault();
   nav.classList.toggle("active");
 });
